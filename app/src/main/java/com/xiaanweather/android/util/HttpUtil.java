ade7ee1fb64f7e9b282245e0d5a524caaacf0511
fangyang <fangyang_91@163.com>
2017-02-27 23:35:24 +0800

    3.1 add HttpUtil

---
+++ app/src/main/java/com/xiaanweather/android/util/HttpUtil.java
@@ -0,0 +1,11 @@
+package com.xiaanweather.android.util;
+
+/**
+ * Description:
+ * Copyright  : Copyright (c) 2016
+ * Author     : yangfang
+ * Date       : 2017/2/27 23:34
+ */
+
+public class HttpUtil {
+}
