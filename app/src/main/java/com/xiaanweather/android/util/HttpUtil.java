fc723aae825ee6db366e3965bf91d204b01bf49a
fangyang <fangyang_91@163.com>
2017-03-01 01:15:54 +0800

    finish the function of privince, city and county

--- app/src/main/java/com/xiaanweather/android/util/HttpUtil.java
+++ app/src/main/java/com/xiaanweather/android/util/HttpUtil.java
@@ -1,5 +1,8 @@
 package com.xiaanweather.android.util;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 /**
  * Description:
  * Copyright  : Copyright (c) 2016
@@ -8,5 +11,10 @@ package com.xiaanweather.android.util;
  */
 
 public class HttpUtil {
-    //实验一次提交
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback){
+        //传入请求地址，并且注册一个回调来处理服务器响应就可以
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
 }
